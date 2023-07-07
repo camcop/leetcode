e15f37bb6f6ece6f0ed8b83f1b4a721f34327577
Cameron Copland <cjcopland@gmail.com>
2023-07-07 14:47:27 +0100

    GroupAnagrams49 solution

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import com.leetcode.GroupAnagrams49;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(GroupAnagrams49.groupAnagrams(strs));
+
+
+    }
+}
