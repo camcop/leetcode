c1ba635c72b9c6fa4809ec52220e8faca680808c
Cam Copland <cjcopland@gmail.com>
2025-05-22 09:34:00 +0100

    refactor 283

--- src/com/leetcode/MoveZeroes283.java
+++ src/com/leetcode/MoveZeroes283.java
@@ -11,12 +11,8 @@ public class MoveZeroes283 {
         for (int j = 0; j < nums.length; j++) {
             if (nums[j] != 0) {
                 nums[i] = nums[j];
+                if (j > i) nums[j] = 0;
                 i++;
-            }
-            if (j == nums.length - 1) {
-                while (i < nums.length) {
-                    nums[i++] = 0;
-                }
             }
         }
     }
@@ -29,7 +25,8 @@ class Runner283 {
     public static void main(String[] args) {
 
         MoveZeroes283 sol = new MoveZeroes283();
-        int[] nums = {0,1,0,3,12};
+//        int[] nums = {0,1,0,3,12};
+        int[] nums = {0, 1};
         sol.moveZeroes(nums);
         System.out.println(Arrays.toString(nums));
 
