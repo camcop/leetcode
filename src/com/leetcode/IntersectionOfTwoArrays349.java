94c820018faa312e8e5501973e52f8b5d59b031d
Cam Copland <cjcopland@gmail.com>
2025-06-02 09:50:06 +0100

    improved solution 349

--- src/com/leetcode/IntersectionOfTwoArrays349.java
+++ src/com/leetcode/IntersectionOfTwoArrays349.java
@@ -7,22 +7,14 @@ import java.util.Set;
 public class IntersectionOfTwoArrays349 {
 
     public int[] intersection(int[] nums1, int[] nums2) {
-
-        Set<Integer> intersection = new HashSet<>();
         Set<Integer> set1 = new HashSet<Integer>(Arrays.stream(nums1).boxed().toList());
         Set<Integer> set2 = new HashSet<Integer>(Arrays.stream(nums2).boxed().toList());
-
+        Set<Integer> set3 = new HashSet<Integer>();
         for (int n : set1) {
-            if (set2.contains(n))
-                intersection.add(n);
+            if (set2.contains(n)) set3.add(n);
         }
 
-        int[] out = new int[intersection.size()];
-        for (int i = 0; i < intersection.size(); i++) {
-            out[i] = (int) intersection.toArray()[i];
-        }
-
-        return out;
+        return set3.stream().mapToInt(Integer::intValue).toArray();
     }
 
 
